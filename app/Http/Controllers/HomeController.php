436b92fef8b727415997c5e41bc1db0d4a5b9b9c
afrinofficial <afrinsultana.seoexpate@gmail.com>
2024-02-04 09:26:13 +0600

    priority wise menu showing complete

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,39 +6,37 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Category;
 
-
-
 class HomeController extends Controller
 {
-   
-        public function menu()
-        {
-            $categories = Category::all();
-    
-            return view('menu', compact('categories'));
-        }
-    
-        public function buildTreeView($categories, $parent = null, $level = 0)
-        {
-            $html = '';
-        
-            $filteredCategories = $categories->where('parent_id', $parent);
-        
-            if ($filteredCategories->count() > 0) {
-                $html .= $level == 0 ? '<ul class="nav navbar-nav">' : '<ul class="dropdown-menu">';
-        
-                foreach ($filteredCategories as $category) {
-                    $html .= '<li><a href="#">' . $category->category . '<span class="caret"></span></a>';
-                    // Use $this-> to reference the method within the class
-                    $html .= $this->buildTreeView($categories, $category->id, $level + 1);
-        
-                    $html .= '</li>';
-                }
-        
-                $html .= '</ul>';
+
+    public function menu()
+    {
+        $categories = Category::all();
+
+        return view('menu', compact('categories'));
+    }
+
+    public function buildTreeView($categories, $parent = null, $level = 0)
+    {
+        $html = '';
+
+        // Filter and sort categories by priority
+        $filteredCategories = $categories->where('parent_id', $parent)->sortBy('priority');
+
+        if ($filteredCategories->count() > 0) {
+            $html .= $level == 0 ? '<ul class="nav navbar-nav">' : '<ul class="dropdown-menu">';
+
+            foreach ($filteredCategories as $category) {
+                $html .= '<li><a href="#">' . $category->category . '<span class="caret"></span></a>';
+                // Use $this-> to reference the method within the class
+                $html .= $this->buildTreeView($categories, $category->id, $level + 1);
+
+                $html .= '</li>';
             }
-        
-            return $html;
+
+            $html .= '</ul>';
         }
-        
+
+        return $html;
+    }
 }
